ffaaa6d5098811cd5de233471bca1871c0b5daa4
Tomás Fox <tomas.c.fox@gmail.com>
2020-05-31 23:00:38 -0300

    Add test configuration

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+/**
+ * @type {import("@jest/types/build/Config").InitialOptions}
+ */
+module.exports = {
+  preset: "ts-jest",
+  testRunner: "jest-circus/runner",
+};
